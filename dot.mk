8c3ad553cfc88b53f530762d6ba5aeb970b5d292
SimplyJeevS <jefsellekaerts@gmail.com>
2018-05-30 16:16:02 +0200

    changed all aosip stuff to dot stuff

--- dot.mk
+++ dot.mk
@@ -20,10 +20,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from oneplus2 device
 $(call inherit-product, device/oneplus/oneplus2/device.mk)
 
-# Inherit some common AOSiP stuff.
-$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
+# Inherit some common DotOS stuff.
+$(call inherit-product, vendor/dot/config/common.mk)
 
-PRODUCT_NAME := aosip_oneplus2
+PRODUCT_NAME := dot_oneplus2
 PRODUCT_DEVICE := oneplus2
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
